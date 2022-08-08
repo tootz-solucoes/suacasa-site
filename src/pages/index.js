92b7daa461cf6e50ff128919df37ff03dda60510
Wendell P. Barreto <wendellp.barreto@gmail.com>
2022-08-08 12:41:59 -0300

    feat: add animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Animated } from 'react-animated-css'
 
 const HomePage = () => {
   const handleOnClick = () => {
@@ -32,63 +33,103 @@ const HomePage = () => {
           <FontAwesomeIcon icon={faArrowUpFromSquare} className="text-sm" />
         </button>
       </div>
-      <div className="h-[120px] w-[120px] md:h-[150px] md:w-[150px]  ">
-        <Image
-          src="/logo-light.png"
-          alt="Sua Casa - Café & Guaraná"
-          width={493}
-          height={408}
-        />
-      </div>
+      <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+        <div className="h-[120px] w-[120px] md:h-[150px] md:w-[150px] ">
+          <Image
+            src="/logo-light.png"
+            alt="Sua Casa - Café & Guaraná"
+            width={493}
+            height={408}
+          />
+        </div>
+      </Animated>
       <div className="flex-col items-center justify-center py-10 max-w-[320px] w-full">
-        <Link href="https://bit.ly/sua-casa-instagram" passHref>
-          <a
-            target="_blank"
-            className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="mr-1 text-lg fa-fw"
-            />
-            Instagram
-          </a>
-        </Link>
-        <Link href="https://bit.ly/sua-casa-whatsapp" passHref>
-          <a
-            target="_blank"
-            className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
-          >
-            <FontAwesomeIcon icon={faWhatsapp} className="mr-1 text-lg fa-fw" />
-            Nosso WhatsApp
-          </a>
-        </Link>
-        <Link href="https://bit.ly/sua-casa-maps" passHref>
-          <a
-            target="_blank"
-            className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
-          >
-            <FontAwesomeIcon
-              icon={faMapLocationDot}
-              className="mr-1 text-lg fa-fw -mt-[3px]"
-            />
-            Onde nos encontrar
-          </a>
-        </Link>
-        <Link href="https://bit.ly/sua-casa-encomendar-kit-guarana" passHref>
-          <a
-            target="_blank"
-            className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
-          >
-            <FontAwesomeIcon
-              icon={faCupStrawSwoosh}
-              className="mr-1 text-lg fa-fw -mt-[4px]"
-            />
-            Encomende seu Kit Guaraná
-          </a>
-        </Link>
+        <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
+          <Link href="https://bit.ly/sua-casa-instagram" passHref>
+            <a
+              target="_blank"
+              className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="mr-1 text-lg fa-fw"
+              />
+              Instagram
+            </a>
+          </Link>
+        </Animated>
+        <Animated
+          animationIn="fadeIn"
+          animationOut="fadeOut"
+          isVisible={true}
+          style={{
+            'animation-delay': '50ms'
+          }}
+        >
+          <Link href="https://bit.ly/sua-casa-whatsapp" passHref>
+            <a
+              target="_blank"
+              className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
+            >
+              <FontAwesomeIcon
+                icon={faWhatsapp}
+                className="mr-1 text-lg fa-fw"
+              />
+              Nosso WhatsApp
+            </a>
+          </Link>
+        </Animated>
+        <Animated
+          animationIn="fadeIn"
+          animationOut="fadeOut"
+          isVisible={true}
+          style={{
+            'animation-delay': '100ms'
+          }}
+        >
+          <Link href="https://bit.ly/sua-casa-maps" passHref>
+            <a
+              target="_blank"
+              className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
+            >
+              <FontAwesomeIcon
+                icon={faMapLocationDot}
+                className="mr-1 text-lg fa-fw -mt-[3px]"
+              />
+              Onde nos encontrar
+            </a>
+          </Link>
+        </Animated>
+        <Animated
+          animationIn="fadeIn"
+          animationOut="fadeOut"
+          isVisible={true}
+          style={{
+            'animation-delay': '150ms'
+          }}
+        >
+          <Link href="https://bit.ly/sua-casa-encomendar-kit-guarana" passHref>
+            <a
+              target="_blank"
+              className="flex items-center justify-center text-center py-3 px-4 border border-2 border-primary shadow-sm text-md font-medium rounded-full text-primary bg-white hover:bg-white focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-white w-full  mb-6"
+            >
+              <FontAwesomeIcon
+                icon={faCupStrawSwoosh}
+                className="mr-1 text-lg fa-fw -mt-[4px]"
+              />
+              Encomende seu Kit Guaraná
+            </a>
+          </Link>
+        </Animated>
       </div>
       <span className="absolute text-center bottom-0 left-0 text-gray-200 text-sm mt-auto pb-6 w-full">
-        Um lugar que você ama estar 💜
+        <Animated
+          animationIn="fadeInUp"
+          animationOut="fadeOut"
+          isVisible={true}
+        >
+          <span> Um lugar que você ama estar 💜</span>
+        </Animated>
       </span>
     </div>
   )
